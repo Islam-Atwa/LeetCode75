4a31d40d2f3090dcee71763289f375d15ad12024
Islam Atwa <160741057+Islam-Atwa@users.noreply.github.com>
2024-11-28 23:32:50 +0200

    [1768] [Time Beats: 68.71%] [Memory Beats: 34.92%] - LeetPush

---
+++ 1768-Merge-Strings-Alternately.js
@@ -0,0 +1,15 @@
+var mergeAlternately = function(word1, word2) {
+    let merged = [];
+
+    for (let i = 0; i < Math.max(word1.length, word2.length); i++) {
+        if (i < word1.length) {
+            merged.push(word1[i]);
+        }
+        
+        if (i < word2.length) {
+            merged.push(word2[i]);
+        }
+    }
+
+    return merged.join("");    
+};
