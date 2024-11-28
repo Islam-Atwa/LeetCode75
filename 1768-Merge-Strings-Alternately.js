86aef45590776a14b9239650c2d10b1787dfa590
Islam Atwa <160741057+Islam-Atwa@users.noreply.github.com>
2024-11-29 00:36:29 +0200

    [1768] [Time Beats: 57.22%] [Memory Beats: 88.89%] - LeetPush

--- 1768-Merge-Strings-Alternately.js
+++ 1768-Merge-Strings-Alternately.js
@@ -1,15 +1,18 @@
+/**
+ * @param {string} word1
+ * @param {string} word2
+ * @return {string}
+ */
 var mergeAlternately = function(word1, word2) {
-    let merged = [];
+    let result = '';
+    for (let i = 0; i < Math.max(word1.length, word2.length); i++)
+    {
+        if (i < word1.length) result += word1[i]; 
 
-    for (let i = 0; i < Math.max(word1.length, word2.length); i++) {
-        if (i < word1.length) {
-            merged.push(word1[i]);
-        }
-        
-        if (i < word2.length) {
-            merged.push(word2[i]);
-        }
+        if (i < word2.length) result +=word2[i]; 
     }
-
-    return merged.join("");    
+    return result;
 };
+
+//  Explanation of the problem in Arabic:
+//  https://www.youtube.com/watch?v=I_gv_tDSvCw&t=249s
